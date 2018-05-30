ae8311c72f697cb3c7c5a31b929cd900140fc0a0
Momomartin <martin.dessart@hotmail.fr>
2018-05-30 15:04:56 +0200

    create player & enemies, associate moving fonction and speed

--- js/app.js
+++ js/app.js
@@ -1,36 +1,95 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+const Enemy = function(x,y) {
     this.sprite = 'images/enemy-bug.png';
+    // Set the enemy position
+    this.x = x;
+    this.y = y;
+    this.multiplier=Math.floor((Math.random()*10)+1);
 };
 
-// Update the enemy's position, required method for game
+
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+  this.x=this.x+this.multiplier*(dt*75);
+  if (this.x>450){
+    this.reset();
+  }
+  if (this.y == player.y && this.x>(player.x-40) && this.x<(player.x+40)){
+    Player.prototype.reset();
+    console.log("yo");
+    }
+}
+Enemy.prototype.reset=function(){
+  this.x=-50;
+  this.multiplier = Math.floor((Math.random() * 5) + 1);
+  let enPos=[60,140,220];
+  this.y=enPos[Math.floor(Math.random()*3)];
+}
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+Enemy.prototype.handleInput = function() {
+};
 
+const Player = function(x,y){
+  this.sprite = 'images/char-boy.png';
+  // Set the player position
+  this.x = x;
+  this.y = y;
+
+}
+
+Player.prototype.update = function(dt) {
+    this.x = this.x*dt;
+    this.y = this.y*dt;
+};
+
+Player.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.update = function(dt) {
+this.x=this.x;
+this.y=this.y;
+}
+
+
+Player.prototype.handleInput = function(move) {
+  if (move ==='up') {
+		this.y = this.y - 80;
+}
+  if (move ==='down') {
+    this.y = this.y + 80;
+  }
+  if (move ==='left') {
+    this.x = this.x - 101;
+  }
+  if (move ==='right') {
+    this.x = this.x + 101;
+  }
+}
+
+Player.prototype.reset = function(){
+  console.log('ok');
+  const player = new Player(405, 380);
+}
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
+const player = new Player(405, 380);
+const allEnemies = [];
+let enPos=[60,140,200];
+let y=enPos[Math.floor(Math.random()*3)];
+let x = -100;
+let enemy1 = new Enemy(x,y);
+let enemy2 = new Enemy(x,y);
+let enemy3 = new Enemy(x,y);
+//let enemy4 = new Enemy(x,y);
+//let enemy5 = new Enemy(x,y);
+//let enemy6 = new Enemy(x,y);
+allEnemies.push(enemy1, enemy2, enemy3);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
